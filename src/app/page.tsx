6e2d50c5c9cecdcb2763f42ececc86c03d647e33
LennySks <david.p2016@outlook.com>
2024-10-07 17:26:52 +0200

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
-import Link from "next/link";
+const mockUrls = [
+  "https://utfs.io/f/W3VPmidSNmjM0zAXdAhexy7VlrXM28Lw4sYEBjKmoqvR0kO1",
+  "https://utfs.io/f/W3VPmidSNmjM0eRN9jhexy7VlrXM28Lw4sYEBjKmoqvR0kO1",
+  "https://utfs.io/f/W3VPmidSNmjM9OIdbAlFHzN4fLm7sU6TAWiy5uGk0orSwevh",
+];
 
-import { LatestPost } from "~/app/_components/post";
-import { api, HydrateClient } from "~/trpc/server";
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
-
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        Hello answerstack!
-      </main>
-    </HydrateClient>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
