b7c647eb165c437937671f59ac7af6b4f84e0478
LennySks <david.p2016@outlook.com>
2024-10-11 15:46:31 +0200

    added sql file + able to show items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,20 @@
+import Image from "next/image";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/W3VPmidSNmjM0zAXdAhexy7VlrXM28Lw4sYEBjKmoqvR0kO1",
-  "https://utfs.io/f/W3VPmidSNmjM0eRN9jhexy7VlrXM28Lw4sYEBjKmoqvR0kO1",
-  "https://utfs.io/f/W3VPmidSNmjM9OIdbAlFHzN4fLm7sU6TAWiy5uGk0orSwevh",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function Home() {
-  const posts = await db.query.posts.findMany();
+  const threads = await db.query.threads.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
-            <img src={image.url} />
+        {[...threads].map((thread, index) => (
+          <div key={thread.id + "-" + index} className="w-48">
+            <img src={thread.image} alt="Image logo" width="80px" />
+            <div key={thread.id}>{thread.name}</div>
           </div>
         ))}
       </div>
