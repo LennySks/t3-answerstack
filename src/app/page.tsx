b2fa90cf01131fc5e02c8c974620f893e0111eee
LennySks <david.p2016@outlook.com>
2024-10-11 16:37:44 +0200

    updated readme + signing in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,35 @@
-import Image from "next/image";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function Home() {
+async function Threads() {
   const threads = await db.query.threads.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {[...threads, ...threads, ...threads].map((thread, index) => (
+        <div key={thread.id + "-" + index} className="w-48">
+          <img src={thread.image} alt="Image logo" width="80px" />
+          <div key={thread.id}>{thread.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default async function Home() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...threads, ...threads, ...threads].map((thread, index) => (
-          <div key={thread.id + "-" + index} className="w-48">
-            <img src={thread.image} alt="Image logo" width="80px" />
-            <div key={thread.id}>{thread.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Threads />
+      </SignedIn>
     </main>
   );
 }
